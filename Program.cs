7158bfe57e8d94d69a7e3b2690c86edc470a826c
xtuxur <alex.delzar@gmail.com>
2017-09-27 18:07:55 -0500

    codicioso

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace codicioso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float cambioQueseDebe;
+
+            do
+            {
+                System.Console.Write("Cuánto cambio se te debe: ");
+                float.TryParse(Console.ReadLine(), out cambioQueseDebe);
+            } while( cambioQueseDebe <= 0 );
+            
+            System.Console.WriteLine($"Número de monedas a devolver: {numeroDeMonedas(cambioQueseDebe)}");
+
+
+        }
+
+        static int numeroDeMonedas(float seDebeCambio)
+        {
+            //float monedaDeCinco = 5.0f;
+            //float monedaDePeso = 1.0f;
+            //float monedaDeCincuetaCentavos = 0.5f;
+            //float mondedaDeDiezCentavos = 0.1f;
+            //float monedaDeUnCentavo = 0.01f;
+            return 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+ # Codicioso
+
+ # TL;DR
+Implementar un programa que calcula el número mínimo de monedas necesarias para dar un cambio al usuario.
+
+Cuánto cambio se te debe: .41
+Número de monedas a devolver: 5
+
+ # Especificación
+* Suponga que las únicas monedas disponibles son monedas de cinco pesos ($5), monedas de un peso ($1), monedas de    cincuenta centavos (50 ¢), monedas de diez centavos (10 ¢), y monedas de un centavo (1 ¢).
